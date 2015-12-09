dbd82b33e8889f0a0ae2e86391ddb330c725611c
Chris Henk <cahenk95@gmail.com>
2015-12-08 21:29:32 -0600

    now uses ConstString's

--- lib/form/reCaptcha.php
+++ lib/form/reCaptcha.php
@@ -12,8 +12,8 @@
 			protected $response;
 			
 			public function __construct($siteKey, $secret){
-				$this->SITE_KEY = $siteKey;
-				$this->SECRET = $secret;
+				$this->SITE_KEY = new \bmca\container\ConstString($siteKey);
+				$this->SECRET = new \bmca\container\ConstString($secret);
 			}
 			
 			//Checks to see if a valid response has been received
@@ -25,7 +25,7 @@
 					filter_var($_POST['g-recaptcha-response'], FILTER_SANITIZE_STRING);
 					
 					//Recaptcha object for verification
-					$recaptcha = new \ReCaptcha\ReCaptcha($this->SECRET);
+					$recaptcha = new \ReCaptcha\ReCaptcha($this->SECRET->value());
 
 					//Verify the response
     				$this->response = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
@@ -51,7 +51,7 @@
 			//Returns the HTML to be included in the form
 			public function getHTML(){
 				return '
-				<div class="g-recaptcha" data-sitekey="'.$this->SITE_KEY.'"></div>
+				<div class="g-recaptcha" data-sitekey="'.$this->SITE_KEY->value().'"></div>
 				<script type="text/javascript"
 						src="https://www.google.com/recaptcha/api.js?hl=<?php echo $lang; ?>">
 				</script>
